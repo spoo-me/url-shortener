2b4c4b4910098405255165ac7ba15745934b13de
Zane <90309290+Zingzy@users.noreply.github.com>
2024-01-13 16:09:25 +0530

    Remove unused variable and condition in shorten_url() function

--- main.py
+++ main.py
@@ -49,7 +49,6 @@ def shorten_url():
     password = request.form.get("password")
     max_clicks = request.form.get("max-clicks")
     alias = request.form.get("alias")
-    json = request.form.get("json")
 
     app.logger.info(f"Received request data: {request.form}")
 
@@ -139,8 +138,6 @@ def shorten_url():
     response = jsonify({"short_url": f"{request.host_url}{short_code}"})
 
     if request.args.get("json") == "true":
-        return response
-    elif json==True:
         return response
     elif (
         "Accept" in request.headers and request.headers["Accept"] == "application/json"
