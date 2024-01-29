6796359c230ac16beb8b7f929ca046c449dedb54
Zane <90309290+Zingzy@users.noreply.github.com>
2024-01-30 00:47:56 +0530

    Update CSS styles

--- static/js/index-script.js
+++ static/js/index-script.js
@@ -2,3 +2,13 @@ function toggleDropdown() {
     const dropdown = document.querySelector('.dropdown');
     dropdown.classList.toggle('dropdown-expanded');
 }
+
+const inputBox = document.querySelector('#alias');
+
+inputBox.addEventListener('focus', (e) => {
+    document.querySelector('.buttonIn').classList.add('focus');
+});
+
+inputBox.addEventListener('blur', (e) => {
+    document.querySelector('.buttonIn').classList.remove('focus');
+});
