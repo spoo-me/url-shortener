bd2e64c501a25bf08a11c7d5a7366a90ebcb1cb8
Zane <90309290+Zingzy@users.noreply.github.com>
2024-02-02 00:12:04 +0530

    Update mobile-header.css and add self-promo section to templates

---
+++ static/js/self-promo.js
@@ -0,0 +1,6 @@
+
+document.querySelector('.self-promo-close').addEventListener('click', closeSelfPromo);
+
+function closeSelfPromo() {
+    document.querySelector('.self-promo').classList.add('hidden');
+}
