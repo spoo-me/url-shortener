51bfc8dafa40d0a0e08a061000e813899f9c9683
Zane <90309290+Zingzy@users.noreply.github.com>
2024-04-03 12:22:11 +0530

    Add self-hosting docs

--- blueprints/docs.py
+++ blueprints/docs.py
@@ -1,19 +1,18 @@
 from flask import Blueprint
 from utils import *
 from .limiter import limiter
+import os
 
 docs = Blueprint("docs", __name__)
 
 
-@docs.route("/docs/<file_name>")
+@docs.route("/docs/<path:file_path>")
 @limiter.exempt
-def serve_docs(file_name):
+def serve_docs(file_path):
     try:
-        ext = file_name.split(".")[1]
-        if ext in ["html"]:
-            return render_template(f"docs/{file_name}", host_url=request.host_url)
-        else:
-            return send_file(f"docs/{file_name}")
+        if not os.path.exists(f"templates/docs/{file_path}.html"):
+            raise FileNotFoundError
+        return render_template(f"docs/{file_path}.html", host_url=request.host_url)
     except:
         return (
             render_template(
@@ -24,7 +23,6 @@ def serve_docs(file_name):
             ),
             404,
         )
-
 
 @docs.route("/legal/privacy-policy")
 @limiter.exempt
