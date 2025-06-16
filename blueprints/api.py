c08e74096fead6ab1e90d2eb6218ce1889127820
Zingzy <90309290+Zingzy@users.noreply.github.com>
2025-06-16 12:29:20 +0530

    Move the old docs and self-hosting gudie to the new docs

--- blueprints/api.py
+++ blueprints/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 from .limiter import limiter
 
 api = Blueprint("api", __name__)
@@ -7,4 +7,13 @@ api = Blueprint("api", __name__)
 @api.route("/api", methods=["GET"])
 @limiter.exempt
 def api_route():
-    return render_template("api.html", host_url=request.host_url)
+    if request.args.get("old") == "1":
+        return render_template(
+            "api.html",
+            host_url=request.host_url,
+            self_promo=True,
+            self_promo_uri="https://docs.spoo.me",
+            self_promo_text="We have moved and revamped the docs to https://docs.spoo.me",
+        )
+    else:
+        return redirect("https://docs.spoo.me/api"), 301
