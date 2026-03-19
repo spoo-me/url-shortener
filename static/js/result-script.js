81c69d52b71688b1eb019d1b6c9ee7725e9af700
zingzy <admin@spoo.me>
2026-03-19 17:32:01 +0530

    feat: refactor QR code download functionality to use fetch API and improve CORS handling

--- static/js/result-script.js
+++ static/js/result-script.js
@@ -11,25 +11,15 @@ function copyShortUrl() {
 
 function downloadQRCode() {
     var qrcode = document.getElementById("qrcode");
-    var qrCodeSrc = qrcode.src;
-
-    var link = document.createElement("a");
-
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = "blob";
-    xhr.onload = function () {
-        var reader = new FileReader();
-        reader.onloadend = function () {
-            link.href = reader.result;  // Use the result from the FileReader
+    fetch(qrcode.src, { mode: "cors" })
+        .then(function (res) { return res.blob(); })
+        .then(function (blob) {
+            var link = document.createElement("a");
+            link.href = URL.createObjectURL(blob);
             link.download = "qrcode.png";
-            document.body.appendChild(link);
             link.click();
-            document.body.removeChild(link);  // Clean up the link element
-        };
-        reader.readAsDataURL(xhr.response);
-    };
-    xhr.open("GET", qrCodeSrc);
-    xhr.send();
+            URL.revokeObjectURL(link.href);
+        });
 }
 
 function shortenAnotherLink() {
